f318242ea2dbdbcfce098af0a53c1a8bbb25da61
Mobina Sharafi <youremail@whatever.com>
2025-11-12 06:29:31 +0000

    Added /usr/33777254 base path for Goldsmiths deployment

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var ejs = require('ejs')
 const app = express();
 app.use(express.urlencoded({ extended: true })); // registration form body parser
 app.use(express.static("public")); // allow Express to serve CSS & images from "public" folder
+app.use('/usr/33777254', express.static("public"))
 
 const port = process.env.PORT || 8000;
 
@@ -15,6 +16,7 @@ app.set('view engine', 'ejs');
 // Load the route handlers
 const mainRoutes = require("./routes/main");  
 app.use('/', mainRoutes);
+app.use('/usr/33777254', mainRoutes);
 
 // Start the web app listening
 app.listen(port, '127.0.0.1', () => console.log(`App running on port ${port}!`));
